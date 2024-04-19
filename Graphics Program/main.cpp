be208a5f4a84dad1574143312699608e159acdff
Joeri <84157343+Diedeliedee@users.noreply.github.com>
2024-04-19 12:19:38 +0200

    Setup cleanup

--- Graphics Program/main.cpp
+++ Graphics Program/main.cpp
@@ -4,37 +4,19 @@
 #include <GLFW/glfw3.h>
 
 //	Forward declaration:
+int Setup(GLFWwindow*& window);			//	Try to find out what the difference is between (Class* Param), and (Class* &Param).
 void processInput(GLFWwindow* window);
+void CreateTriangle();
+void CreateShader();
 
 int main()
 {
-	//	Initialize GLFW.
-	if (!glfwInit())
-	{
-		std::cout << "ERROR: GLFW initialization failed." << std::endl;
-	}
+	//	Initialize the window.
+	GLFWwindow* window = NULL;
+	if (Setup(window) < 0) return -1;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	//	Create a GLFW window.
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Graphics Program", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "ERROR: Failed to create GLFW window." << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	//	Load GLAD.
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "ERROR: Failed to load Glad." << std::endl;
-		return -1;
-	}
+	CreateTriangle();
+	CreateShader();
 
 	//	Create a viewport.
 	glViewport(0, 0, 1280, 720);
@@ -57,6 +39,49 @@ int main()
 	//	Close the application.
 	glfwTerminate();
 	return 0;
+}
+
+int Setup(GLFWwindow*& window)
+{
+	//	Initialize GLFW.
+	if (!glfwInit())
+	{
+		std::cout << "ERROR: GLFW initialization failed." << std::endl;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//	Create a GLFW window.
+	window = glfwCreateWindow(1280, 720, "Graphics Program", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "ERROR: Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//	Load GLAD.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "ERROR: Failed to load Glad." << std::endl;
+		return -1;
+	}
+
+	return 1;
+}
+
+void CreateTriangle()
+{
+
+}
+
+void CreateShader()
+{
+
 }
 
 void processInput(GLFWwindow* window)
